505c223919f284163e464e0553a0827f1c99aade
Gleb Bakhmetyev <bakhmetyevgv@gmail.com>
2021-02-09 19:26:39 +0300

    Use repeat() instead of for loop

--- source/chess.js
+++ source/chess.js
@@ -1,14 +1,11 @@
-const chess = N => {
+const chess = (N) => {
     if (N <= 1 || !Number.isInteger(Number(N)))
         return null;
 
-    let res = '';
-    for (let j = 0; j < N; ++j) {
-        for (let i = 0; i < N; ++i)
-            res += (j + i) % 2 === 0 ? '*' : ' ';
-        
-        res += '\n';
-    }
+    let board = '';
+    let rowBlack = '* '.repeat(N / 2).concat(N % 2 === 0 ? '' : '*').concat('\n')
+    let rowWhite = ' *'.repeat(N / 2).concat(N % 2 === 0 ? '' : ' ').concat('\n');
+    board += (rowBlack + rowWhite).repeat(N / 2).concat(N % 2 === 0 ? '' : rowBlack);
 
-    return res;
+    return board;
 }
